38ca42c25abfef23c23c89de49da2934927052d3
Krekoten <nikita.krekoten@codex-soft.com>
2023-03-26 00:15:33 +0200

    chore: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
+import pinia from './plugins/pinia'
 
-createApp(App).mount('#app')
+createApp(App).use(pinia).mount('#app')
