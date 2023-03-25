99c6467aa95749e1f019c7c0838a198ad45a048c
Krekoten <nikita.krekoten@codex-soft.com>
2023-03-26 00:16:31 +0200

    chore: create alias '@' for src

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -13,4 +14,9 @@ export default defineConfig({
       fix: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
