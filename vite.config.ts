1b5ef18d47fabbee8a3c8d9190de13b73b6df97d
Krekoten <nikita.krekoten@codex-soft.com>
2023-03-26 00:27:37 +0200

    chore: start server with port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import stylelint from 'vite-plugin-stylelint'
 import path from 'path'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
   plugins: [
     vue(),
     eslintPlugin({
